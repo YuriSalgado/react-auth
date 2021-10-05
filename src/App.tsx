307d0ff85cc1c6e84651f30d69e02b4dd93dbdd1
yuri salgado <yuri_salgadinho@hotmail.com>
2021-10-05 20:04:06 -0300

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">Login</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Register</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Nav/>
+      
       <main className="form-signin">
-        <form>
-         <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-          <div className="form-floating">
-            <input type="email" className="form-control" placeholder="name@example.com"/>
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" placeholder="Password"/>
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-        </form>
+        <Login />
       </main>
     </div>
   );
