3a8f2da5b07c43331b0a094609d2c96920431238
yuri salgado <yuri_salgadinho@hotmail.com>
2021-10-06 20:18:43 -0300

    add router + add register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      
-      <main className="form-signin">
-        <Login />
-      </main>
+      <BrowserRouter>
+        <Nav/>
+        
+        <main className="form-center">
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            Register!
-        </div>
+        <form>
+            <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+            <div className="form-floating">
+                <input type="text" className="form-control" placeholder="name lastname" />
+                <label htmlFor="floatingInput">Name</label>
+            </div>
+            <div className="form-floating">
+                <input type="email" className="form-control" placeholder="name@example.com" />
+                <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" placeholder="Password" />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+            <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
     );
 };
 
